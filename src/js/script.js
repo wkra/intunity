f57b69750fb2d5e04ca94b123580a2f3e7e188a3
wkra <wkrawiec@gmail.com>
2017-09-02 13:30:25 +0200

    1.7
    some changes, google file added

--- src/js/script.js
+++ src/js/script.js
@@ -3,19 +3,19 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // smooth scroll
   $(function() {
-  $('nav a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
+    $('nav a[href*="#"]:not([href="#"])').click(function() {
+      if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html, body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
       }
-    }
+    });
   });
-});
   // end smooth scroll
 
 //   // social media icons
@@ -26,8 +26,8 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
   // parallax effect
+  $('#header').parallax({imageSrc: './img/bg.jpg'});
   $('.parallax-window').parallax({imageSrc: './img/photobg.jpg'}); 
-  $('.parallax-window2').parallax({imageSrc: './img/bg.jpg'});
   //end parralax
 
   // intunity team section function
@@ -39,7 +39,8 @@ document.addEventListener("DOMContentLoaded", function () {
       type: "GET",
       cache: true,
       success: function (data, status, error) {
-        intunityTeamData = sort(data, sortCity);
+        intunityTeamData = data;
+        // intunityTeamData = sort(data, sortCity);
         printTeam(intunityTeamData);
       },
       error: function (data, status, error) {
@@ -49,26 +50,26 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     var intunityTeamData = [];
-    var sortCity = ['Kraków', 'Wrocław', 'Łódź', 'Warszawa'];
+    // var sortCity = ['Kraków', 'Wrocław', 'Łódź', 'Warszawa'];
 
-    // sort data by sortCity
-    function sort(data, template){
-      var newData = [];
-      for (var i = 0; i < template.length; i++){
-        for (var ii = 0; ii < data.length; ii++){
-          if (template[i] === data[ii].city){
-            newData.push(data[ii])
-          }
-        }
-      }
-      return newData;
-    };
+    // // sort data by sortCity
+    // function sort(data, template){
+    //   var newData = [];
+    //   for (var i = 0; i < template.length; i++){
+    //     for (var ii = 0; ii < data.length; ii++){
+    //       if (template[i] === data[ii].city){
+    //         newData.push(data[ii])
+    //       }
+    //     }
+    //   }
+    //   return newData;
+    // };
 
      // print team
     function printTeam(data){
       var currCity = '';
       var city = '<div class="city"><h2>%city%</h2></div><div class="city--team"></div>';
-      var person = '<div class="person %personEvenOdd%"><div class="person--photo"><img src="./img/default-person/smallphoto.jpg" alt="%nameAlt%" class="img-responsive"></div><div class="person--name"><h3>%name%</h3><h4>%instrument%</h4><h5 class="read-more" data-moreid="%moreid%">więcej...</h5></div></div><div class="person-text %contentid%"></div>';
+      var person = '<div class="person"><div class="person--photo"><img src="./img/default-person/smallphoto.jpg" alt="%nameAlt%" class="img-responsive"></div><div class="person--name"><h3>%name%</h3><h4>%instrument%</h4><h5 class="read-more" data-moreid="%moreid%">więcej...</h5></div></div><div class="person-text"></div>';
 
       for (var i = 0; i < data.length; i++){
         if (data[i].city !== currCity){
@@ -80,69 +81,150 @@ document.addEventListener("DOMContentLoaded", function () {
           $('.intunityTeamContent').append(newCity)
         }
         var newPerson = '';
-        if (i%2 === 0){
-          newPerson = person.replace('%personEvenOdd%', 'personEven');
-        }
-        if (i%2 !== 0){
-          newPerson = person.replace('%personEvenOdd%', 'personOdd');
-        }
+        newPerson = person.replace('%name%', data[i].name);
+        newPerson = newPerson.replace('%instrument%', data[i].instrument);
+        newPerson = newPerson.replace('%nameAlt%', data[i].name);
+
+        // search small photo
         if (data[i].smallphoto !== '' && data[i].url !== ''){
           newPerson = newPerson.replace('default-person', data[i].url);
           newPerson = newPerson.replace('smallphoto.jpg', data[i].smallphoto);
         }
-        newPerson = newPerson.replace('%nameAlt%', data[i].name);
-        newPerson = newPerson.replace('%name%', data[i].name);
-        newPerson = newPerson.replace('%instrument%', data[i].instrument);
-
+        // if no person text remove "więcej"
         if (data[i].text === ''){
           newPerson = newPerson.replace('<h5 class="read-more" data-moreid="%moreid%">więcej...</h5>', '');
         }
         if (data[i].text !== ''){
           newPerson = newPerson.replace('%moreid%', i);
-          
         }
-        newPerson = newPerson.replace('%contentid%', 'id'+i);
+        // print person
         $('.city--team:last').append(newPerson);
 
       }
     }
 
-    var bigPhoto = '<img src="./img/%url%/%bigphoto%" alt="%alt%" class="img-responsive">';
+
     //event listener
-    $('.intunityTeamContent').click(function(el){
-      if (el.target.className === 'read-more'){
-        var id = Number(el.target.attributes[1].value);
-        if (intunityTeamData[id].bigphoto !== ''){
-          var newBigPhoto = bigPhoto.replace('%url%', intunityTeamData[id].url);
-          newBigPhoto = newBigPhoto.replace('%bigphoto%', intunityTeamData[id].bigphoto);
-          newBigPhoto = newBigPhoto.replace('%alt%', intunityTeamData[id].name);
-          $('.id'+id).html("<h2>" + intunityTeamData[id].name + "</h2>" + newBigPhoto + intunityTeamData[id].text).addClass('person-opened');
-        } else {
-          $('.id'+id).html("<h2>" + intunityTeamData[id].name + "</h2>" + intunityTeamData[id].text).addClass('person-opened');
-        }
-       
-      }
-      // var windowWidth = $(window).width()
-      // if (el.target.className === 'read-more'){
-      //   var id = Number(el.target.attributes[1].value);
-      //   if (windowWidth >= 768 && id%2 === 0){
-      //     $('.id'+(id+1)).html("<h2>" + intunityTeamData[id].name + "</h2>" + intunityTeamData[id].text).addClass('person-opened')
-      //   } else {
-      //     $('.id'+id).html("<h2>" + intunityTeamData[id].name + "</h2>" + intunityTeamData[id].text).addClass('person-opened')
-      //   }
+    $('.intunityTeamContent').on( 'click', '.read-more', readMoreFunc);    
+    
+    // read more function
+    function readMoreFunc(){
+
+      // bigphoto template
+      var bigPhoto = '<img src="./img/%url%/%bigphoto%" alt="%alt%" class="img-responsive">';
+      var newBigPhoto = '';
+
+      //more text change
+      var changeMore = 'zwiń...';
+      var originalMore = 'więcej...';
+
+      // get id
+      var id = Number($(this).attr('data-moreid'));
+
+      // classes
+      var openClass = 'person-opened';
+      var closeClass = 'person-closed';
+      
+      // put bigphoto
+      if (intunityTeamData[id].bigphoto !== ''){
+        newBigPhoto = bigPhoto.replace('%url%', intunityTeamData[id].url);
+        newBigPhoto = newBigPhoto.replace('%bigphoto%', intunityTeamData[id].bigphoto);
+        newBigPhoto = newBigPhoto.replace('%alt%', intunityTeamData[id].name);
+      } 
+      // first click
+      if (!$(this).parent().parent().next().hasClass(closeClass) && !$(this).parent().parent().next().hasClass(openClass)){
         
-      // }
+        // put text
+        $(this).parent().parent().next()
+        .addClass(openClass)
+        .html('<h2><strong>' + intunityTeamData[id].name + '</strong></h2>' + newBigPhoto + intunityTeamData[id].text);
+        
+        //change read more
+        $(this).text(changeMore);
 
-    })
-    // $('.intunityTeamContent').on( "click", ".read-more", function() {
-    //   console.log($( this ).parent().parent().next())
-    // });
-    // $('.intunityTeamContent').on( "click", ".read-more", function() {
-    //   console.log($( this ).parent().parent().next())
-    // });
+      } else {
+        // next clicks
 
-  })()
+        // toggle class
+        $(this).parent().parent().next()
+        .toggleClass(openClass).toggleClass(closeClass);
+
+        // toggle read more
+        if ($(this).text() === changeMore){
+          $(this).text(originalMore);
+        } else {
+          $(this).text(changeMore);
+        }
+      }
+    }
+    // end read more function
+
+
+
+  })();
   // end intunity team
+
+  // OBJECT PARALLAX EFFECT
+(function(){
+  
+    var windowHeight = $(window).height();
+    var windowScrollPosTop = $(window).scrollTop();
+    var windowScrollPosBottom = windowHeight + windowScrollPosTop;
+    var windowWidth = $(window).width();
+  
+    // start position flag
+    var startFlagScroll = false;
+  
+    function parallaxEffectFunc(){
+      windowHeight = $(window).height();
+      windowScrollPosTop = $(window).scrollTop();
+      windowScrollPosBottom = windowHeight + windowScrollPosTop;
+      windowWidth = $(window).width();
+  
+      // do not animate on small screen
+      if (windowWidth < 768){
+        myScrollVal(0, 0, '.photo-1>img');
+        myScrollVal(0, 0, '.photo-2>img');
+      } else {
+        // SET OBJECT
+        myScrollVal(-40, 40, '.photo-1>img');
+        myScrollVal(30, -30, '.photo-2>img');
+
+        //set start position
+        if (startFlagScroll === false){
+          startFlagScroll = true;
+        }
+      }
+
+  
+    };
+    
+    parallaxEffectFunc();
+    $(window).scroll(parallaxEffectFunc);
+  
+    function myScrollVal(startValue, endValue, object, direction){
+      if (direction === undefined){
+        direction = 'Y';
+      }
+
+      var objectOffset = $(object).offset();
+      var objectOffsetTop = objectOffset.top;
+      var objectOffsetBottom = objectOffsetTop + $(object).outerHeight();
+  
+      if (windowScrollPosBottom > objectOffsetTop && windowScrollPosTop < (objectOffsetTop+$(object).height())){
+  
+  //      var scrollTop = $(this).scrollTop();
+        $(object).css('transform', 'translate' + direction + '(' + Math.round((startValue+(((windowScrollPosBottom-objectOffsetTop)*(endValue-startValue))/(windowHeight+(objectOffsetBottom-objectOffsetTop))))) + '%)');
+      } 
+      else if (startFlagScroll === false){
+  //      $(this).scrollTop();
+        $(object).css('transform', 'translate' + direction + '(' + startValue + '%)');
+      }
+  
+    };
+  
+  })();
+    //end OBJECT PARALLAX EFFECT
 
 // end DOMContentLoaded
 });
